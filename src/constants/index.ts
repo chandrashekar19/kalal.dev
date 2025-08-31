42e3897e6324244e2533b6cf9aa60930df1e8189
KalalChandu <kalalshannu19@gmail.com>
2025-08-31 18:18:00 +0530

    updated: the package.json file

---
+++ src/constants/index.ts
@@ -0,0 +1,128 @@
+
+  export const projects = [
+    {
+      title: "Business Analytics Dashboard",
+      description:
+        "A responsive and performance-focused admin dashboard for tracking business KPIs, visualizing analytics, and managing users and activity data. Built with a focus on speed, usability, and real-time insights.",
+      image: "/images/buisness.png",
+      technologies: [
+        "React",
+        "Zustand",
+        "Tailwind CSS",
+        "Recharts",
+        "shadcn",
+        "JWT Auth",
+      ],
+      liveUrl: "https://biz-command.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/biz-command",
+      featured: true,
+    },
+
+    {
+      title: "Task Manager",
+      description:
+        "A real-time Kanban-style task management app with drag-and-drop boards and offline persistence using Redux. Designed for teams to collaborate and organize work visually.",
+      image: "/images/task-board.png",
+      technologies: ["React", "zustand", "Tailwind CSS", "dnd-kit"],
+      liveUrl: "https://nimble-boards.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/nimble-boards",
+      featured: true,
+    },
+    {
+      title: "Job Tracker & Resume Builder",
+      description:
+        "An all-in-one job application tracker with resume builder and cover letter generator. Built to help job seekers manage applications and create personalized career documents efficiently.",
+      image: "/images/Job-board.png",
+      technologies: [
+        "React",
+        "Zustand",
+        "Tailwind CSS",
+        "shadcn",
+        "html2pdf",
+      ],
+      liveUrl: "https://apply-and-build.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/apply-and-build",
+      featured: true,
+    },
+    {
+      title: "Snapgram",
+      description:
+        "A modern social media app with a native mobile feel, infinite scroll, and smooth performance. Built using React, TypeScript, Appwrite, and React Query for real-time updates and seamless UX.",
+      technologies: [
+        "React",
+        "TypeScript",
+        "Appwrite",
+        "Tailwind CSS",
+        "React Query",
+        "shadcn"
+      ],
+      image: "/images/snapgram.png",
+      liveUrl: "https://snapgram-orcin-nu.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/social-media-app",
+      featured: true,
+    },
+     {
+      title: "Chatter fusion",
+      description:
+        "A real-time chat application built using React, Tailwind CSS, Zustand, and Socket.IO. Users can join rooms and send messages instantly.",
+      technologies: [
+        "React js",
+        "JavaScript",
+        "tailwind css",
+        "node js",
+        "zustand",
+        "socket.io",
+      ],
+      image: "images/chatter.png",
+      liveUrl: "https://chatter-stream.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/chatter-fusion",
+      featured: false,
+    },
+     {
+      title: "Health Care Application",
+      description:
+        "A healthcare patient management application that allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments",
+      technologies: [
+        "Next js",
+        "Typescript",
+        "tailwind css",
+        "React Query",
+        "Appwrite",
+        "shadcn"
+      ],
+      image: "images/health-care.png",
+      liveUrl: "https://health-care-amber.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/health-care",
+      featured: false,
+    },
+    {
+        title: "Blogs Website",
+      description:
+        "A modern, responsive tech blog application built with React, TypeScript, and Tailwind CSS with category filtering, search functionality, and beautiful code syntax highlighting",
+      technologies: [
+        "React js",
+        "Typescript",
+        "markdown", 
+        "shadcn"
+      ],
+      image: "images/blogs.png",
+      liveUrl: "https://kalal-dev-blogs.vercel.app/blog",
+      githubUrl: "https://github.com/chandrashekar19/kalal.dev-blogs",
+      featured: false,
+    },
+     {
+        title: "Auth Explorer",
+      description:
+        "An educational resource designed to help developers understand modern authentication concepts like **OAuth 2.0** and **JWT (JSON Web Tokens)** with interactive examples and visual guides.",
+      technologies: [
+        "React js",
+        "Typescript",
+        "tailwind css", 
+        "shadcn"     
+      ],
+      image: "images/auth-explorer.png",
+      liveUrl: "https://auth-explorer.vercel.app/",
+      githubUrl: "https://github.com/chandrashekar19/kalal.dev-blogs",
+      featured: false,
+    },
+  ];
